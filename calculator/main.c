db0a3021cf14aa7bac0f4a8050b0bddeea8369ee
alexandre <contact@alexandreconseil.fr>
2023-12-20 23:40:38 +0100

    Improve code architecture

--- calculator/main.c
+++ calculator/main.c
@@ -1,6 +1,8 @@
 #include "mathematics/mathematics.h"
 #include "algorithm/algorithm.h"
 
+#define N 10000
+
 // #include <stdio.h>
 // #include <math.h>
 // #include <time.h>
@@ -21,19 +23,18 @@
 } */
 
 int main(void) {
-	int N = 500000;
 	// printf("Start with %d decimals\n", N);
 
-	pi(N);
+	// pi(N);
 
 	// clock_t t0 = clock();
-	// for (int i = 0; i < N; i++) {
-		// pi(i);
+	for (int i = 0; i < N; i++) {
+		pi(i);
 
 		// long double pi_n = pi(i);
 		// to_hex(pi_n, hex);
 		// printf("> i: %d | pi: %c\n", i, hex[0]);
-	// }
+	}
 	// clock_t t1 = clock();
 
 	// printf("\nTIME: %f seconds\n", (double)(t1 - t0) / CLOCKS_PER_SEC);
